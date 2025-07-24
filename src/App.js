9b7dade03bacdccf0a09f58f55950ff8046e0ade
MaheryRandrianirina <maheryrandrianirina2000@gmail.com>
2025-07-24 12:56:23 +0300

    feat: display result in a table

--- src/App.js
+++ src/App.js
@@ -24,50 +24,58 @@ class App {
 
         program.command(`${COMMAND_NAME} <zipPath>`)
             .description(COMMAND_DESCRIPTION)
-            .action((zipPath) => {
-                if (!fs.existsSync(zipPath)) {
-                    console.error(`Le fichier ${zipPath} n'existe pas.`)
-                    return
-                }
-                
-                try {
-                    const fileBuffer = fs.readFileSync(zipPath)
-                    const files = unzipSync(fileBuffer)
-                    const outDir = path.join(process.cwd(), path.parse(zipPath).name)
-    
-                    // verify if the zip contains the expected files
-                    const missingFiles = this.#filesInTheZip.filter(file => !Object.keys(files).includes(file));
-                    if (missingFiles.length > 0) {
-                        console.warn(`❌ Attention : Le fichier ZIP ne contient pas les fichiers suivants : ${missingFiles.join(', ')}.`);
-                        return
-                    }
-
-                    
-                    for (const [filename, data] of Object.entries(files)) {
-                        if( !this.#filesInTheZip.includes(filename)) {
-                            console.warn(`Le fichier ${filename} n'est pas reconnu et ne sera pas extrait.`);
-                            continue
-                        }
-
-                        new PdfReader().parseBuffer(data, (err, item) => {
-                            if (err) {
-                                console.error("Error:", err)
-                            }else if (item && item.text) {
-                                const dateMatch = item.text.match(/(\d{1,2}\/\d{1,2}\/\d{4})/)
-                                if (dateMatch && dateMatch < new Date().toLocaleDateString('fr-FR')) {
-                                    console.warn(`Le fichier ${filename} contient une date de validité expirée : ${dateMatch[0]}.`);
-                                }
-                            };
-                        });
-                    }
-                }catch(e) {
-                    console.error('❌ Une erreur est survenue lors de l\'extraction du ZIP:', e.message);
-                }
-
-            })
+            .action(this.#actionFromCommand.bind(this))
 
         program.parse(process.argv)
 
+    }
+
+    async #actionFromCommand(zipPath) {
+        if (!fs.existsSync(zipPath)) {
+            console.error(`Le fichier ${zipPath} n'existe pas.`)
+            return
+        }
+
+        try {
+            const fileBuffer = fs.readFileSync(zipPath)
+            const files = unzipSync(fileBuffer)
+
+            // verify if the zip is missing files
+            const missingFiles = this.#filesInTheZip.filter(file => !Object.keys(files).includes(file));
+            if (missingFiles.length > 0) {
+                console.warn(`❌ Attention : Le fichier ZIP ne contient pas les fichiers suivants : ${missingFiles.join(', ')}.`);
+                return
+            }
+
+            let filesContent = []
+            
+            for (const [filename, data] of Object.entries(files)) {
+                if (!this.#filesInTheZip.includes(filename)) {
+                    console.warn(`Le fichier ${filename} n'est pas reconnu et ne sera pas extrait.`);
+                    continue
+                }
+
+                await new Promise((resolve, reject) => {
+                    new PdfReader().parseBuffer(data, (err, item) => {
+                        if (err) {
+                            reject(err)
+                        } else if (item && item.text) {
+                            filesContent.push({Fichiers: filename, Contenu: item.text});
+
+                            const dateMatch = item.text.match(/(\d{1,2}\/\d{1,2}\/\d{4})/)
+                            if (dateMatch && dateMatch < new Date().toLocaleDateString('fr-FR')) {
+                                console.warn(`⚠️  Le fichier "${filename}" contient une date de validité expirée : ${dateMatch[0]}.`);
+                            }
+
+                            resolve(true)
+                        };
+                    });
+                })
+            }
+            console.table(filesContent)
+        } catch (e) {
+            console.error('❌ Une erreur est survenue lors de l\'extraction du ZIP:', e.message);
+        }
     }
 }
 
