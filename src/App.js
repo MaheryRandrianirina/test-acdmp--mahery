f9a3ef71a1b12bcdaa851c647a6dd19c5a2de964
MaheryRandrianirina <maheryrandrianirina2000@gmail.com>
2025-07-24 15:35:49 +0300

    feat!: update icon

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App {
             // verify if the zip is missing files
             const missingFiles = this.#filesInTheZip.filter(file => !Object.keys(files).includes(file));
             if (missingFiles.length > 0) {
-                console.warn(`⚠️  Attention : Le fichier ZIP ne contient pas les fichiers suivants : ${missingFiles.join(', ')}.`);
+                console.warn(`❌ Attention : Le fichier ZIP ne contient pas les fichiers suivants : ${missingFiles.join(', ')}.`);
             }
 
             /**
